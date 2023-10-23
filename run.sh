f2713b1e67b9ed3a0a7eae60f01f7b2971cea2b5
hexbabe <sean.yu@viam.com>
2023-10-23 13:42:20 -0400

    Add more logs

--- run.sh
+++ run.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
+echo "[Script] Setting up module."
+
 cd "$(dirname "$0")"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if apt list --installed | grep -q "python3-pip"; then
-        echo "[Script] python3-pip installation found"
+        echo "[Script] python3-pip installation found."
     else
-        echo "[Script] python3-pip installation not found; attempting to install python3-pip"
+        echo "[Script] python3-pip installation not found. Attempting to install python3-pip."
         sudo apt update
         sudo apt install python3-pip
     fi
 
     if apt list --installed | grep -q "python3-venv"; then
-        echo "[Script] python3-venv installation found"
+        echo "[Script] python3-venv installation found."
     else
-        echo "[Script] python3-venv installation not found; attempting to install python3-venv"
+        echo "[Script] python3-venv installation not found. Attempting to install python3-venv."
         sudo apt update
         sudo apt install python3-venv
     fi
 fi
 
 if [ -f "$(pwd)/.installed" ]; then
+    echo "[Script] Dependencies installed. Activating venv."
     source viam-env/bin/activate
 else
-    echo "[Script] Installing virtual environment and dependencies..."
+    echo "[Script] Installing virtual environment and dependencies."
     python3 -m pip install --user virtualenv
     python3 -m venv viam-env
     source viam-env/bin/activate
@@ -32,5 +35,6 @@ else
     fi
 fi
 
+echo "[Script] Setup complete. Starting module process."
 # Uses `exec` so that termination signals reach the Python process; handled by Stoppable protocol
 exec python3 -m src.main "$@"
