be42cf9eccd55b87792734a0f81973b7999f3a20
hexbabe <sean.yu@viam.com>
2023-10-23 12:30:44 -0400

    Attempt more syntax fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-cd `dirname $0`
+#!/bin/bash
+cd "$(dirname "$0")"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if apt list --installed | grep -q "python3-pip"; then
@@ -13,21 +13,18 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     fi
 fi
 
-
-if [ -f .installed ]
-  then
+if [ -f .installed ]; then
     source viam-env/bin/activate
-  else
+else
     python3 -m pip install --user virtualenv
     python3 -m venv viam-env
     source viam-env/bin/activate
     pip3 install --upgrade -r requirements.txt
-    if [ $? -eq 0 ]
-      then
+    if [ $? -eq 0 ]; then
         touch .installed
     fi
 fi
 
-# Be sure to use `exec` so that termination signals reach the python process,
+# Be sure to use `exec` so that termination signals reach the Python process,
 # or handle forwarding termination signals manually
-exec python3 -m src.main $@
+exec python3 -m src.main "$@"
