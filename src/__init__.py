af9cf91d2a50fa320ffc58b8588c9059f0d1f385
sean yu <55464069+hexbabe@users.noreply.github.com>
2023-11-21 12:58:30 -0500

    Add packaging for aarch64 (appimage) (#12)
    * Add pyproject.toml
    
    * Add recipe
    
    * Add scripts to build file, but the binary isn't finding the entrypoint correctly
    
    * Works now, but still only responding to sigkill

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ This file registers the model with the Python SDK.
 from viam.components.camera import Camera
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 
-from .oak_d import OakDModel
+from src.oak_d import OakDModel
 
 Registry.register_resource_creator(
     Camera.SUBTYPE,
