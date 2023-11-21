db8b7d2b01cf6e9416193a8fbbf294429e00ca95
hexbabe <sean.xiaochen.yu@gmail.com>
2023-11-21 13:43:44 -0500

    Address Sean P's venv echo comment and more in run.sh

--- run.sh
+++ run.sh
@@ -5,21 +5,21 @@ cd "$(dirname "$0")"
 VENV_NAME="viam-oak-d-venv"
 PYTHON="$VENV_NAME/bin/python"
 LOG_PREFIX="[Viam OAK-D setup]"
-ENV_ERROR="$LOG_PREFIX Error: this module requires Python >=3.8.1, pip3, and virtualenv to be installed."
+
+echo "$LOG_PREFIX Starting the Viam OAK-D camera module. Using this script requires Python >=3.8.1, pip3, and venv to be installed."
 
 if ! python3 -m venv "$VENV_NAME" >/dev/null 2>&1; then
-    echo "$LOG_PREFIX Warning: failed to create virtualenv."
-    echo "$ENV_ERROR" >&2
+    echo "$LOG_PREFIX Error: failed to create venv. Please use your system package manager to install python3-venv." >&2
     exit 1
 else
-    echo "$LOG_PREFIX Created/found virtualenv."
+    echo "$LOG_PREFIX Created/found venv."
 fi
 
 # Remove -U if viam-sdk should not be upgraded whenever possible
 # -qq suppresses extraneous output from pip
 echo "$LOG_PREFIX Installing/upgrading Python packages."
 if ! "$PYTHON" -m pip install -r requirements.txt -Uqq; then
-    echo "$LOG_PREFIX Error: pip failed to install requirements.txt. Please make sure pip is properly installed on your machine." >&2
+    echo "$LOG_PREFIX Error: pip failed to install requirements.txt. Please use your system package manager to install python3-pip." >&2
     exit 1
 fi
 
