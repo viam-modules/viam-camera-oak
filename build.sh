5f9aed59e9a5afa6dbe2be0ba0f4522edc99f43c
Michael Lee <michaellee1019@gmail.com>
2024-01-29 12:54:07 -0500

    add ls debugging

--- build.sh
+++ build.sh
@@ -14,5 +14,6 @@ fi
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
+ls -R /root/project/.venv/lib/python3.11/site-packages/ahrs
 python3 -m PyInstaller --add-data /root/project/.venv/lib/python3.11/site-packages/ahrs/utils/WMM2020:ahrs/utils/WMM2020 --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
