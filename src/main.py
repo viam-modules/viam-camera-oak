10058c98cf2e588a394cc72f1e4c6e0a520d80ca
Michael Lee <michaellee1019@gmail.com>
2024-01-30 10:50:39 -0500

    remove init

--- src/main.py
+++ src/main.py
@@ -4,15 +4,20 @@ from viam.components.camera import Camera
 from viam.logging import getLogger
 from viam.module.module import Module
 from src.oak_d import OakDModel
-
+from viam.resource.registry import Registry, ResourceCreatorRegistration
 
 LOGGER = getLogger(__name__)
-
 
 async def main():
     """This function creates and starts a new module, after adding all desired resources.
     Resources must be pre-registered. For an example, see the `__init__.py` file.
     """
+Registry.register_resource_creator(
+    Camera.SUBTYPE,
+    OakDModel.MODEL,
+    ResourceCreatorRegistration(OakDModel.new, OakDModel.validate),
+)
+
     module = Module.from_args()
     module.add_model_from_registry(Camera.SUBTYPE, OakDModel.MODEL)
     LOGGER.debug("Starting module in main.py.")
