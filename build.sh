b34036987888ea3c5c9016eb293de0ad2ce80566
Michael Lee <michaellee1019@gmail.com>
2024-02-02 10:47:43 -0500

    try separate site pkg dirs

--- build.sh
+++ build.sh
@@ -5,15 +5,17 @@ if [ "$UNAME" = "Linux" ]
 then
     echo "Installing venv on Linux"
     sudo apt-get install -y python3.11-venv
+    SITE_PKG=/root/project/.venv/lib/python3.11/site-packages/ahrs
 fi
 if [ "$UNAME" = "Darwin" ]
 then
     echo "Installing venv on Darwin"
     brew install python3.11-venv
+    SITE_PKG=~/Library/Python/3.11/lib/python/site-packages
 fi
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
-ls -R /root/project/.venv/lib/python3.11/site-packages/ahrs
-python3 -m PyInstaller --add-data /root/project/.venv/lib/python3.11/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
+ls -R 
+python3 -m PyInstaller --add-data $(SITE_PKG)/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
