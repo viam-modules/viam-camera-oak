4d8566f5a0c1877f2e96a554b110329d821fe3af
Michael Lee <michaellee1019@gmail.com>
2024-02-02 13:21:57 -0500

    wrong variable format

--- build.sh
+++ build.sh
@@ -17,5 +17,5 @@ python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
 ls -R 
-python3 -m PyInstaller --add-data $(SITE_PKG)/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
+python3 -m PyInstaller --add-data $SITE_PKG/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
