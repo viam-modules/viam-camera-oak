f025cb41864b64c24ba0761d9d711603313103c0
Michael Lee <michaellee1019@gmail.com>
2024-02-02 14:48:40 -0500

    universal relative path

--- build.sh
+++ build.sh
@@ -5,16 +5,14 @@ if [ "$UNAME" = "Linux" ]
 then
     echo "Installing venv on Linux"
     sudo apt-get install -y python3.11-venv
-    SITE_PKG=/root/project/.venv/lib/python3.11/site-packages
 fi
 if [ "$UNAME" = "Darwin" ]
 then
     echo "Installing venv on Darwin"
     brew install python3.11-venv
-    SITE_PKG=.venv/lib/python/site-packages
 fi
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m PyInstaller --add-data $SITE_PKG/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
+python3 -m PyInstaller --add-data .venv/lib/python3.11/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
