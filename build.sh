a09d2922a0b2914b7351d327197ef289eb10e76b
Michael Lee <michaellee1019@gmail.com>
2024-02-02 14:37:06 -0500

    use relative path for mac

--- build.sh
+++ build.sh
@@ -5,17 +5,16 @@ if [ "$UNAME" = "Linux" ]
 then
     echo "Installing venv on Linux"
     sudo apt-get install -y python3.11-venv
-    SITE_PKG=/root/project/.venv/lib/python3.11/site-packages/ahrs
+    SITE_PKG=/root/project/.venv/lib/python3.11/site-packages
 fi
 if [ "$UNAME" = "Darwin" ]
 then
     echo "Installing venv on Darwin"
     brew install python3.11-venv
-    SITE_PKG=~/Library/Python/3.11/lib/python/site-packages
+    SITE_PKG=.venv/lib/python/site-packages
 fi
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
-ls -R 
 python3 -m PyInstaller --add-data $SITE_PKG/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
