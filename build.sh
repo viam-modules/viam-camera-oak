2ef4be34a3766d0122feb750760ee021a75ae283
hexbabe <sean.xiaochen.yu@gmail.com>
2024-04-05 16:36:11 +0000

    Fix build.sh to account for new cloud runners

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@ UNAME=$(uname -s)
 if [ "$UNAME" = "Linux" ]
 then
     echo "Installing venv on Linux"
-    sudo apt-get install -y python3.11-venv
+    sudo apt-get install -y python3-venv
 fi
 if [ "$UNAME" = "Darwin" ]
 then
     echo "Installing venv on Darwin"
-    brew install python3.11-venv
+    brew install python3-venv
 fi
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m PyInstaller --add-data .venv/lib/python3.11/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
+python3 -m PyInstaller --add-data .venv/lib/python3.10/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
