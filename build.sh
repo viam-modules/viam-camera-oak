3dcbeee1c08bae4b905fef1c9875e95fc290dd7b
hexbabe <sean.xiaochen.yu@gmail.com>
2024-04-05 17:21:16 -0400

    More robust exit in bash build script

--- build.sh
+++ build.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 set -e
 
+error_exit()
+{
+    # Check if the script exits with a non-zero status
+    EXIT_STATUS=$?
+    if [ "$EXIT_STATUS" -ne 0 ]; then
+        echo "Script exited due to an error (status code: $EXIT_STATUS). Please check the output for error messages."
+    fi
+}
+
+# Trap EXIT signal to call error_exit function
+trap error_exit EXIT
+
 UNAME=$(uname -s)
 
-if [ "$UNAME" = "Linux" ]
-then
+if [ "$UNAME" = "Linux" ]; then
     echo "Installing venv on Linux"
     sudo apt-get install -y python3-venv
-fi
-if [ "$UNAME" = "Darwin" ]
-then
+elif [ "$UNAME" = "Darwin" ]; then
     echo "Installing venv on Darwin"
     brew install python3-venv
+else
+    echo "Unsupported operating system: $UNAME"
+    exit 1
 fi
+
 python3 -m venv .venv
-. .venv/bin/activate
+source .venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m PyInstaller --add-data .venv/lib/python3.11/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
+python3 -m PyInstaller --add-data ".venv/lib/python3.11/site-packages/ahrs/utils:ahrs/utils" --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
