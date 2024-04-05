e3874b7659f6e304d8229ffc830c669b0d505ce7
hexbabe <sean.xiaochen.yu@gmail.com>
2024-04-05 12:16:11 -0400

    Make init changes

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+UNAME=$(uname -s)
+
+if [ "$UNAME" = "Linux" ]
+then
+    echo "Installing venv on Linux"
+    sudo apt-get install -y python3.11-venv
+fi
+if [ "$UNAME" = "Darwin" ]
+then
+    echo "Installing venv on Darwin"
+    brew install python3.11-venv
+fi
+python3 -m venv .venv
+. .venv/bin/activate
+pip3 install -r requirements.txt
+python3 -m PyInstaller --add-data .venv/lib/python3.11/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
+tar -czvf dist/archive.tar.gz dist/main
