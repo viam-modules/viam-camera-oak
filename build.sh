1a7e67654faf40875015ea48d60dc46d67a3c36f
hexbabe <sean.xiaochen.yu@gmail.com>
2024-04-05 15:34:09 -0400

    Try Python3.11

--- build.sh
+++ build.sh
@@ -14,5 +14,5 @@ fi
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m PyInstaller --add-data .venv/lib/python3.10/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
+python3 -m PyInstaller --add-data .venv/lib/python3.11/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
