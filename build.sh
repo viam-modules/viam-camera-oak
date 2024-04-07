b1937a4d791775909c31c7b49997d143daed9e24
hexbabe <sean.xiaochen.yu@gmail.com>
2024-04-07 14:35:43 -0400

    Revert back to original script but only change linux apt-get line

--- build.sh
+++ build.sh
@@ -1,41 +1,18 @@
 #!/bin/bash
+UNAME=$(uname -s)
 
-# Desired Python version
-DESIRED_PYTHON_VERSION="3.11"
-
-# Function to find Python path
-find_python() {
-    which python$1 || which python3 || which python || return 1
-}
-
-# Check for the desired Python version and get its path
-PYTHON_PATH=$(find_python $DESIRED_PYTHON_VERSION)
-if [ $? -ne 0 ]; then
-    echo "Python $DESIRED_PYTHON_VERSION is not installed."
-    exit 1
+if [ "$UNAME" = "Linux" ]
+then
+    echo "Installing venv on Linux"
+    sudo apt-get install -y python3-venv
 fi
-
-echo "Using Python at $PYTHON_PATH"
-
-# Check Python version explicitly
-VERSION_CHECK=$($PYTHON_PATH -c "import platform; print(platform.python_version().startswith('$DESIRED_PYTHON_VERSION'))")
-if [ $VERSION_CHECK != "True" ]; then
-    echo "The Python version is not $DESIRED_PYTHON_VERSION."
-    exit 1
+if [ "$UNAME" = "Darwin" ]
+then
+    echo "Installing venv on Darwin"
+    brew install python3.11-venv
 fi
-
-# Create virtual environment with the found Python
-$PYTHON_PATH -m venv .venv
-source .venv/bin/activate
-
-# Ensure pip is installed in the venv
-python -m ensurepip
-
-# Install requirements
-pip install -r requirements.txt
-
-# Use the Python executable from the venv for PyInstaller
-python -m PyInstaller --add-data .venv/lib/python$DESIRED_PYTHON_VERSION/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
-
-# Create a distribution archive
+python3 -m venv .venv
+. .venv/bin/activate
+pip3 install -r requirements.txt
+python3 -m PyInstaller --add-data .venv/lib/python3.11/site-packages/ahrs/utils:ahrs/utils --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
