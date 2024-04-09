66c93d627dfe04793bb9c5f33e2ccc08e09de010
hexbabe <sean.xiaochen.yu@gmail.com>
2024-04-09 15:51:25 -0400

    Dummy commit

--- build.sh
+++ build.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
 UNAME=$(uname -s)
-
-if [ "$UNAME" = "Linux" ]; then
+if [ "$UNAME" = "Linux" ]
+then
     echo "Installing venv on Linux"
-    export DEBIAN_FRONTEND=noninteractive
-    sudo apt-get update && sudo apt-get install -y --no-install-recommends software-properties-common
-    sudo add-apt-repository ppa:deadsnakes/ppa -y
-    sudo apt-get update
-    sudo apt-get install -y --no-install-recommends python3.12
-    
-    # sudo apt-get install -y --no-install-recommends python3.12-venv
-    
-elif [ "$UNAME" = "Darwin" ]; then
+    # sudo apt-get install -y python3-venv
+fi
+if [ "$UNAME" = "Darwin" ]
+then
     echo "Installing venv on Darwin"
-    # brew update && brew upgrade
-    # brew reinstall python@3.12
-    # brew link --overwrite python@3.12
+    # brew reinstall python3
+    # brew install python3-venv
 fi
 
-python3.12 -m venv .venv
+python3 -m venv .venv
 source .venv/bin/activate
-pip3 install -r requirements.txt
-python3 -m PyInstaller --add-data "$(python3 -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')/ahrs/utils:ahrs/utils" --onefile --hidden-import="googleapiclient" src/main.py
+python3 -m pip install -r requirements.txt
+# Dynamically find the Python version directory inside .venv/lib
+PYTHON_LIB_PATH=$(find .venv/lib -type d -name "python3.*" -print -quit)
+AHRS_UTILS_PATH="$PYTHON_LIB_PATH/site-packages/ahrs/utils"
+python3 -m PyInstaller --add-data "$AHRS_UTILS_PATH:ahrs/utils" --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
