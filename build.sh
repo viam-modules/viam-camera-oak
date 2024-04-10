b1abe84323c47131584e6a46fd2450c280e9701f
hexbabe <sean.xiaochen.yu@gmail.com>
2024-04-10 13:27:35 -0400

    Clean up rest of appimage leftover deprecated stuff; Add back run.sh as it is necessary for unit tests

--- build.sh
+++ build.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-UNAME=$(uname -s)
-if [ "$UNAME" = "Linux" ]
-then
-    echo "Installing venv on Linux"
-    # sudo apt-get install -y python3-venv
-fi
-if [ "$UNAME" = "Darwin" ]
-then
-    echo "Installing venv on Darwin"
-    # brew reinstall python3
-    # brew install python3-venv
-fi
-
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+
+# Create a virtual environment to run our code
+VENV_NAME=".venv"
+PYTHON="$VENV_NAME/bin/python"
+LOG_PREFIX="[Viam OAK local setup]"
+
+echo "$LOG_PREFIX Starting the Viam OAK camera module. Using this script requires Python >=3.8.1, pip3, and venv to be installed."
+
+if ! python3 -m venv "$VENV_NAME" >/dev/null 2>&1; then
+    echo "$LOG_PREFIX Error: failed to create venv. Please use your system package manager to install python3-venv." >&2
+    exit 1
+else
+    echo "$LOG_PREFIX Created/found venv."
+fi
+
+# Remove -U if viam-sdk should not be upgraded whenever possible
+# -qq suppresses extraneous output from pip
+echo "$LOG_PREFIX Installing/upgrading Python packages."
+if ! "$PYTHON" -m pip install -r requirements.txt -qq; then
+    echo "$LOG_PREFIX Error: pip failed to install requirements.txt. Please use your system package manager to install python3-pip." >&2
+    exit 1
+fi
+
+# Be sure to use `exec` so that termination signals reach the python process,
+# or handle forwarding termination signals manually
+echo "$LOG_PREFIX Starting module."
+exec "$PYTHON" -m src.main "$@"
